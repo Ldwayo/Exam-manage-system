21183938429ddef5c7ac3cb9755816c4b6a30ba2
Ldwayo <lindongwangyu@qq.com>
2018-07-03 14:51:39 +0800

    add main form

---
+++ team-work/team-work/Form_main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace team_work
+{
+    public partial class Form_main : Form
+    {
+        public Form_main()
+        {
+            InitializeComponent();
+        }
+
+        private void 信息管理ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
