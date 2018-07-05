0b56a61982cf41affec0bbf9de2176236989e5fe
m13691183379_2 <m13691183379_2@163.com>
2018-07-05 10:17:00 +0800

    Update by PeiXiaodong

--- team-work/team-work/Back_Book.cs
+++ team-work/team-work/Back_Book.cs
@@ -16,5 +16,10 @@ namespace team_work
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
